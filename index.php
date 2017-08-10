1bda707bc6cda3f3bb6cc03c51ba2eb36e37dcf2
Kevin Yan <k3vinyan@gmail.com>
2017-08-10 01:09:41 -0700

    add google excel sheet

--- index.php
+++ index.php
@@ -4,98 +4,106 @@
 <head>
 
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    
+
     <title>Chat</title>
-    
+
     <link rel="stylesheet" href="style.css" type="text/css" />
-    
+
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
     <script type="text/javascript" src="chat.js"></script>
     <script type="text/javascript">
-    
-        // ask user for name with popup prompt    
+
+        // ask user for name with popup prompt
         var name = prompt("Enter your chat name:", "Guest");
-        
+
         // default name is 'Guest'
     	if (!name || name === ' ') {
-    	   name = "Guest";	
+    	   name = "Guest";
     	}
-    	
+
     	// strip tags
     	name = name.replace(/(<([^>]+)>)/ig,"");
-    	
+
     	// display name on page
     	$("#name-area").html("You are: <span>" + name + "</span>");
-    	
+
     	// kick off chat
         var chat =  new Chat();
     	$(function() {
-    	
-    		 chat.getState(); 
-    		 
+
+    		 chat.getState();
+
     		 // watch textarea for key presses
-             $("#sendie").keydown(function(event) {  
-             
-                 var key = event.which;  
-           
-                 //all keys including return.  
+             $("#sendie").keydown(function(event) {
+
+                 var key = event.which;
+
+                 //all keys including return.
                  if (key >= 33) {
-                   
-                     var maxLength = $(this).attr("maxlength");  
-                     var length = this.value.length;  
-                     
+
+                     var maxLength = $(this).attr("maxlength");
+                     var length = this.value.length;
+
                      // don't allow new content if length is maxed out
-                     if (length >= maxLength) {  
-                         event.preventDefault();  
-                     }  
-                  }  
+                     if (length >= maxLength) {
+                         event.preventDefault();
+                     }
+                  }
     		 																																																});
     		 // watch textarea for release of key press
-    		 $('#sendie').keyup(function(e) {	
-    		 					 
-    			  if (e.keyCode == 13) { 
-    			  
+    		 $('#sendie').keyup(function(e) {
+
+    			  if (e.keyCode == 13) {
+
                     var text = $(this).val();
-    				var maxLength = $(this).attr("maxlength");  
-                    var length = text.length; 
-                     
-                    // send 
-                    if (length <= maxLength + 1) { 
-                     
-    			        chat.send(text, name);	
+    				var maxLength = $(this).attr("maxlength");
+                    var length = text.length;
+
+                    // send
+                    if (length <= maxLength + 1) {
+
+    			        chat.send(text, name);
     			        $(this).val("");
-    			        
+
                     } else {
-                    
+
     					$(this).val(text.substring(0, maxLength));
-    					
-    				}	
-    				
-    				
+
+    				}
+
+
     			  }
              });
-            
+
     	});
     </script>
 
 </head>
 
 <body onload="setInterval('chat.update()', 1000)">
+  <a href="https://docs.google.com/spreadsheets/d/1HzCH7VzzCe9ERJJ2d9YCRqIafJPWIkdLf1oAbx7c2LQ/edit?usp=sharing">click here to edit
+  </a>
+    <span>
+      <iframe src="https://docs.google.com/spreadsheets/d/1HzCH7VzzCe9ERJJ2d9YCRqIafJPWIkdLf1oAbx7c2LQ/pubhtml?widget=true&amp;headers=false" height="950" width="370"></iframe>
+    </span>
 
-    <div id="page-wrap">
-    
+
+
+    <!-- <span id="page-wrap">
+
         <h2>jQuery/PHP Chat</h2>
-        
+
         <p id="name-area"></p>
-        
+
         <div id="chat-wrap"><div id="chat-area"></div></div>
-        
+
         <form id="send-message-area">
             <p>Your message: </p>
             <textarea id="sendie" maxlength = '100' ></textarea>
         </form>
-    
-    </div>
+
+    </span> -->
+
 
 </body>
 
